fcc078c60191d650c41947726ab71ef2049ee57b
Robert <32489347+Robert-Shen@users.noreply.github.com>
2017-12-12 17:42:09 -0500

    remove credential data in config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-AWS_Access_Key_Id = "AKIAI5RAVTGQXIUN3K5A"
-AWS_Secret_Access_Key = "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"
+AWS_Access_Key_Id = ""
+AWS_Secret_Access_Key = ""
 
-INSTANCE_ID = "i-02688cf0cd3ef25d0"
-PUBLIC_DNS = "ec2-35-153-120-14.compute-1.amazonaws.com"
-PUBLIC_IP = "35.153.120.14"
+INSTANCE_ID = ""
+PUBLIC_DNS = ""
+PUBLIC_IP = ""
 
 KEY_PAIR_FILE_NAME = "login"
